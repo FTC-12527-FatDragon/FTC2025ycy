a0b9dcd04aaef0a23bde4b2d3afcb889099ce4f0
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2024-12-17 21:58:17 +0800

    Gammabot: fixed bug in Elevator (needs test)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestTeleOp.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.ConditionalCommand;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.arcrobotics.ftclib.command.WaitUntilCommand;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.function.Supplier;
+import org.firstinspires.ftc.teamcode.commands.TeleopDriveCommand;
+import org.firstinspires.ftc.teamcode.lib.roadrunner.drive.opmode.LocalizationTest;
+import org.firstinspires.ftc.teamcode.subsystems.Elevator;
+import org.firstinspires.ftc.teamcode.subsystems.Lift;
+import org.firstinspires.ftc.teamcode.subsystems.LiftClaw;
+import org.firstinspires.ftc.teamcode.subsystems.SlideSuperStucture;
+import org.firstinspires.ftc.teamcode.subsystems.drivetrain.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.utils.FunctionalButton;
+
+@TeleOp(name = "Test TeleOp")
+public class TestTeleOp extends CommandOpMode {
+    private Elevator elevator;
+
+    @Override
+    public void initialize() {
+        this.telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        elevator = new Elevator(hardwareMap);
+        elevator.elevate();
+        telemetry.addLine("Elevator finished!");
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
@@ -3,25 +3,25 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.StartEndCommand;
 import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Elevator extends SubsystemBase {
-    final ServoEx elevator;
+    final CRServo elevator;
     public Elevator(HardwareMap hardwareMap){
-        elevator = hardwareMap.get(ServoEx.class, "elevatorMotor");
+        elevator = hardwareMap.get(CRServo.class, "elevatorMotor");
     }
 
     public void elevate(){
-        elevator.setPosition(1);
+        elevator.setPower(1);
     }
 
     public void decline(){
-        elevator.setPosition(0);
+        elevator.setPower(-1);
     }
 
     public void stop(){
-        elevator.setPosition(0.5);
+        elevator.setPower(0);
     }
 
     public Command elevateCommand(){
