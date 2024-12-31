933445da7819b8e0ee0a3218e37ce0388945d63d
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2024-12-31 14:28:25 +0800

    Added servoUtil

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/ServoUtils.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.ConditionalCommand;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ServoUtils {
+    public static Command setServoPosCommand(Servo servo, double pos, long delay) {
+        return new ConditionalCommand(
+                new InstantCommand(
+                        () -> {
+                            servo.setPosition(pos);
+                        })
+                        .andThen(new WaitCommand(delay)),
+                new InstantCommand(() -> {}),
+                () -> servo.getPosition() != pos);
+    }
+}
