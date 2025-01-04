f8c84a8c15aa41de9f7f2e9025dfbedac5716e8c
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2025-01-04 08:43:52 +0800

    Old chamber 1+3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/Chamber1Plus3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/Chamber1Plus3.java
@@ -27,7 +27,6 @@ import org.firstinspires.ftc.teamcode.subsystems.AlphaSlide;
 import org.firstinspires.ftc.teamcode.subsystems.Lift;
 import org.firstinspires.ftc.teamcode.subsystems.drivetrain.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.utils.Pose2dHelperClass;
-import org.firstinspires.ftc.teamcode.utils.Translation2dHelperClass;
 
 @Config
 @Autonomous(name = "Chamber 1+3", group = "Autos")
@@ -42,17 +41,11 @@ public class Chamber1Plus3 extends LinearOpMode {
   public static Pose2dHelperClass chamber = new Pose2dHelperClass(5.49, -29.5, 90.00);
   public static Pose2dHelperClass chamber1 = new Pose2dHelperClass(chamber.X - gap, chamber.Y, 90.00);
   public static Pose2dHelperClass chamber2 =
-      new Pose2dHelperClass(chamber.X - gap * 2, chamber.Y, 90.00);
+          new Pose2dHelperClass(chamber.X - gap * 2, chamber.Y, 90.00);
   public static Pose2dHelperClass chamber3 =
-      new Pose2dHelperClass(chamber.X - gap * 3, chamber.Y, 90.00);
+          new Pose2dHelperClass(chamber.X - gap * 3, chamber.Y, 90.00);
   public static Pose2dHelperClass chamber4 =
           new Pose2dHelperClass(chamber.X - gap * 4, chamber.Y, 90.00);
-
-  public static Translation2dHelperClass Sample1 = new Translation2dHelperClass(49.22, -26.44);
-  public static Translation2dHelperClass Sample2 = new Translation2dHelperClass(59.77, -26.08);
-
-
-  public static Pose2dHelperClass sample1Observation = new Pose2dHelperClass(48.46, -53, 90);
 
   //  public static double startX = 24.43;
   //  public static double startY = -64.95;
@@ -75,15 +68,6 @@ public class Chamber1Plus3 extends LinearOpMode {
                     .alongWith(chamberToGrab(lift, liftClaw))
     );
   }
-  
-  public Command pushBlocksCycle(TrajectorySequence grab2DropSequence, TrajectorySequence drop2Next){
-    return new SequentialCommandGroup(
-            slide.grabCommand(),
-            new AutoDriveCommand(drive, grab2DropSequence),
-            new InstantCommand(slide::openIntakeClaw),
-            new AutoDriveCommand(drive, drop2Next)
-    );
-  }
 
   @Override
   public void runOpMode() throws InterruptedException {
@@ -98,14 +82,12 @@ public class Chamber1Plus3 extends LinearOpMode {
 
     drive = new SampleMecanumDrive(hardwareMap);
 
-    TrajectorySequence push2Blocks = drive.trajectorySequenceBuilder(new Pose2d(2.54, -32.08, Math.toRadians(90.00)))
-            .lineToConstantHeading(new Vector2d(14.17, -38.86))
+    TrajectorySequence push2Blocks = drive.trajectorySequenceBuilder(new Pose2d(1.37, -49.37, Math.toRadians(90.00)))
+            .lineToConstantHeading(new Vector2d(18.69, -31.62))
             .splineToConstantHeading(new Vector2d(34.62, -27.69), Math.toRadians(90.00))
             .splineToConstantHeading(new Vector2d(44.77, -14.54), Math.toRadians(-70.00))
-            .lineToLinearHeading(sample1Observation.toPose2d(), getVelocityConstraint(40, MAX_ANG_VEL, TRACK_WIDTH), SampleMecanumDrive.getACCEL_CONSTRAINT())
-            .lineToLinearHeading(sample1Observation.toPose2d().plus(new Pose2d(0, 2, 0)))
-            .splineToConstantHeading(new Vector2d(54.23, -16.62), Math.toRadians(0))
-            .splineToConstantHeading(Sample2.toVector2d(), Math.toRadians(-90.00))
+            .lineToLinearHeading(new Pose2d(48.46, -53, Math.toRadians(90.00)), getVelocityConstraint(40, MAX_ANG_VEL, TRACK_WIDTH), SampleMecanumDrive.getACCEL_CONSTRAINT())
+            .splineToConstantHeading(new Vector2d(57.69, -15.46), Math.toRadians(-45.00))
             .lineToLinearHeading(new Pose2d(59.08, -53, Math.toRadians(90.00)), getVelocityConstraint(50, MAX_ANG_VEL, TRACK_WIDTH), SampleMecanumDrive.getACCEL_CONSTRAINT())
 //            .splineToSplineHeading(new Pose2d(64.15, -14.54, Math.toRadians(180.00)), Math.toRadians(0.00))
 //            .lineToLinearHeading(new Pose2d(64.15, -56.31, Math.toRadians(180.00)))
@@ -158,43 +140,43 @@ public class Chamber1Plus3 extends LinearOpMode {
     // Push all three samples to the observation zone
     // Repeatedly score the high chamber with slightly different
     CommandScheduler.getInstance()
-        .schedule(
-            new SequentialCommandGroup(
-                initialize(liftClaw, slide),
-                new InstantCommand(() -> drive.setPoseEstimate(startToChamber.start())),
-                liftClaw.closeClawCommand(),
-                new AutoDriveCommand(drive, startToChamber).alongWith(new WaitCommand(Grab2ChamberUpperDelay).andThen(toPreHang(lift, liftClaw))),
+            .schedule(
+                    new SequentialCommandGroup(
+                            initialize(liftClaw, slide),
+                            new InstantCommand(() -> drive.setPoseEstimate(startToChamber.start())),
+                            liftClaw.closeClawCommand(),
+                            new AutoDriveCommand(drive, startToChamber).alongWith(new WaitCommand(Grab2ChamberUpperDelay).andThen(toPreHang(lift, liftClaw))),
 
-                upToChamber(lift),
+                            upToChamber(lift),
 
 //                    stowArmFromBasket(lift, liftClaw),
 
-                new AutoDriveCommand(drive, push2Blocks).alongWith(chamberToGrab(lift, liftClaw)),
+                            new AutoDriveCommand(drive, push2Blocks).alongWith(chamberToGrab(lift, liftClaw)),
 //                    new WaitCommand(500).deadlineWith(lift.manualResetCommand()),
 //
-                new AutoDriveCommand(drive, pushToGrab),
-                        //.alongWith(new WaitCommand(500).deadlineWith(lift.manualResetCommand()))
+                            new AutoDriveCommand(drive, pushToGrab),
+                            //.alongWith(new WaitCommand(500).deadlineWith(lift.manualResetCommand()))
 
-                obersvationToChamberCycle(grabToChamber1, chamberToGrab),
+                            obersvationToChamberCycle(grabToChamber1, chamberToGrab),
 
-                obersvationToChamberCycle(grabToChamber2, chamberToGrab),
+                            obersvationToChamberCycle(grabToChamber2, chamberToGrab),
 
-                obersvationToChamberCycle(grabToChamber3, chamberToGrab)
+                            obersvationToChamberCycle(grabToChamber3, chamberToGrab)
 
-                //                                    .andThen(new
-                // InstantCommand(liftClaw::closeClaw))
-                //                            ,
-                //                            new AutoDriveCommand(drive, trajectory3)
-                //                                    .alongWith(grabToPreHang(lift, liftClaw))
-                //                                    .andThen(new WaitCommand(300))
-                //                                    .andThen(upToChamber(lift))
-                //                                    .andThen(new WaitCommand(500))
-                //                                    .andThen(chamberToGrab(lift, liftClaw))
+                            //                                    .andThen(new
+                            // InstantCommand(liftClaw::closeClaw))
+                            //                            ,
+                            //                            new AutoDriveCommand(drive, trajectory3)
+                            //                                    .alongWith(grabToPreHang(lift, liftClaw))
+                            //                                    .andThen(new WaitCommand(300))
+                            //                                    .andThen(upToChamber(lift))
+                            //                                    .andThen(new WaitCommand(500))
+                            //                                    .andThen(chamberToGrab(lift, liftClaw))
 
-                //                ,
-                //                        new AutoDriveCommand(drive, chamberToFirst),
-                //                        new AutoDriveCommand(drive, firstToObservation),
-                ));
+                            //                ,
+                            //                        new AutoDriveCommand(drive, chamberToFirst),
+                            //                        new AutoDriveCommand(drive, firstToObservation),
+                    ));
 
     waitForStart();
 
