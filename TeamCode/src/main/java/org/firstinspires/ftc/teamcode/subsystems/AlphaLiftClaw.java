29a0363857e989706dadc773acca5891765957c7
AmuroRX-93 <leonyuzhewang@icloud.com>
2025-01-16 15:53:30 +0800

    爪子松紧优化

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AlphaLiftClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AlphaLiftClaw.java
@@ -23,7 +23,7 @@ public class AlphaLiftClaw extends SubsystemBase {
   private final Telemetry telemetry;
 
   public static double LiftClaw_Open = currentRobot==DriveConstants.RobotType.ALPHA?0.7:0.35;
-  public static double LiftClaw_Close = currentRobot==DriveConstants.RobotType.ALPHA?0.33:0.58;
+  public static double LiftClaw_Close = currentRobot==DriveConstants.RobotType.ALPHA?0.33:0.52;
 
   public static long LiftClaw_SwitchDelay = 150;
 
