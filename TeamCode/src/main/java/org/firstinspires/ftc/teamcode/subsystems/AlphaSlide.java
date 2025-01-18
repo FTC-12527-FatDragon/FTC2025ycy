dd4a5d993e713a693fe12220ab0098168b519eb1
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2025-01-18 16:30:45 +0800

    Alphabot: Handoff stable (liftArmServo NEEDS to be changed)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AlphaSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AlphaSlide.java
@@ -39,7 +39,7 @@ public class AlphaSlide extends MotorPIDSlideSubsystem{
   public static long waitGrabTimeout3 = 700;
 
   public static long slideRetractFar = 500;
-  public static long slideRetractNear = 100;
+  public static long slideRetractNear = 150;
 
   private static double turnAngleDeg = 0;
   private TurnServo turnServo = TurnServo.DEG_0;
