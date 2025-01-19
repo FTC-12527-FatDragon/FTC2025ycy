6d8da6d6264dec4843a3ea526edfd142c5a48e50
zihanpeter <luzihan1104@163.com>
2025-01-19 15:52:06 +0800

    Servo Pos + Timeout

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/AutoCommandBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/AutoCommandBase.java
@@ -162,7 +162,6 @@ public abstract class AutoCommandBase extends LinearOpMode {
 
   public Command grabAndBack3() {
     return new SequentialCommandGroup(
-
             slide.autoGrabCommand3A(),
             new InstantCommand(slide::autoForwardSlideExtension),
             new WaitCommand(slideRetractFar),
