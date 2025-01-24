e769a926c8109370aea5349ff6731b50a1f5c74d
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2025-01-24 13:47:16 +0800

    wyz's changes
    Co-Authored-By: AmuroRX-93 <181320375+AmuroRX-93@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AlphaLiftClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AlphaLiftClaw.java
@@ -129,7 +129,7 @@ public class AlphaLiftClaw extends SubsystemBase {
   }
 
   public enum ServoPositions {
-    STOW(currentRobot==DriveConstants.RobotType.ALPHA?0.76:0.83, LiftClaw_Close, currentRobot==DriveConstants.RobotType.ALPHA?0.64:0.615),
+    STOW(currentRobot==DriveConstants.RobotType.ALPHA?0.76:0.82, LiftClaw_Close, currentRobot==DriveConstants.RobotType.ALPHA?0.64:0.615),
     CHAMBER(currentRobot==DriveConstants.RobotType.ALPHA?0.58:0.66, LiftClaw_Close, currentRobot==DriveConstants.RobotType.ALPHA?0.27:0.47),
     BASKET(currentRobot==DriveConstants.RobotType.ALPHA?0.37:0.47, LiftClaw_Close, currentRobot==DriveConstants.RobotType.ALPHA?0.35:0.54),
     GRAB(currentRobot==DriveConstants.RobotType.ALPHA?0.15:0.207, LiftClaw_Open, currentRobot==DriveConstants.RobotType.ALPHA?0.08:0.35);
