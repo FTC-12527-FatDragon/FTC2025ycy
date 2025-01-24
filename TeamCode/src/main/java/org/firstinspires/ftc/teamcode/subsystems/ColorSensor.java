9077905664bc639913254ae47a0ab47c9ae8bc14
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2025-01-24 13:47:39 +0800

    Another part of color sensor code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ColorSensor.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+
+public class ColorSensor extends SubsystemBase {
+    private final ColorRange redRange = ColorRange.RED;//new ColorRange(ColorSpace.HSV, new Scalar(276, ));
+    private final ColorRange yellowRange = ColorRange.YELLOW;
+    private final ColorRange blueRange = ColorRange.BLUE;
+    private final NormalizedColorSensor sensor;
+    public ColorSensor(HardwareMap hardwareMap, String hardwareName) {
+        this(hardwareMap, hardwareName, 2);
+    }
+    public ColorSensor(HardwareMap hardwareMap, String hardwareName, float gain) {
+        sensor = hardwareMap.get(NormalizedColorSensor.class, hardwareName);
+        sensor.setGain(gain);
+    }
+    public boolean objectNear() {
+        return sensor.getNormalizedColors().alpha > 0.5;
+    }
+    public boolean isBlue() {
+        return false;
+    }
+}
